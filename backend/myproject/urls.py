82e415b244a775dfb94fed54efe5f90a9166c39b
Jonabuev <abuovdamir223@gmail.com>
2025-09-01 12:03:36 +0500

    1

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -4,8 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 
+from backend.rentapp.views.auth import health_check
+
 urlpatterns = [
     path('api/', include('rentapp.urls')),
+    path("health/", health_check),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Add format suffix patterns for flexible formats (e.g., /profile.json)
