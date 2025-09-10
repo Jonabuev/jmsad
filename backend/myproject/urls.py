55cf3cc29f6014761bb238b21df9e0078092af69
MaksCool <76480568+MaksCool@users.noreply.github.com>
2025-09-10 15:58:02 +0500

    правильный юрл для серва

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -1,16 +1,15 @@
-# urls.py
-from django.urls import path, include
+# urls.py 
+from django.urls import path, include, re_path 
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf import settings
 from django.conf.urls.static import static
-
-from .views import health_check
-
-
+from django.views.static import serve
+from .views import health_check 
 urlpatterns = [
     path('api/', include('rentapp.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path("health/", health_check),
+    path("accounts", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 # Add format suffix patterns for flexible formats (e.g., /profile.json)
 urlpatterns = format_suffix_patterns(urlpatterns)
