26f8d1e366339b1b205cbee23683c7d2cb1bffc9
MaksCool <76480568+MaksCool@users.noreply.github.com>
2025-10-30 18:09:48 +0500

    Revert "1"
    This reverts commit 59b84dd14b2fc5b9e08bd89d4b2bf7478287f395.

--- backend/rentapp/views/complaint.py
+++ backend/rentapp/views/complaint.py
@@ -351,9 +351,9 @@ def complaint_reasons(request):
     serializer = ComplaintReasonSerializer(reasons, many=True)
     return Response(serializer.data)
 
-
+from rest_framework.permissions import AllowAny
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([AllowAny])
 def all_complaint_reasons(request):
     """
     API endpoint для получения всех причин жалоб с поддержкой мультиязычности.
